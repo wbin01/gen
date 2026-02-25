b9d7d2cc45d20c60e18f5e420dd3b55118b3cc85
wbin01 <wildenalisson@outlook.com>
2026-02-25 02:17:47 -0300

    Cell style class

--- cell/button.py
+++ cell/button.py
@@ -10,8 +10,7 @@ class Button(Cell):
     def __init__(
             self, text: str = '',
             x: int = 0, y: int = 0, width: int = 100, height: int = 32,
-            elided: bool = False, style_class: str = None,
-            *args, **kwargs) -> None:
+            elided: bool = False, *args, **kwargs) -> None:
         """..."""
         super().__init__(*args, **kwargs)
         self.__text = text
@@ -20,30 +19,50 @@ class Button(Cell):
         self.width = width
         self.height = height
         self.__elided = elided
-        self.__style_class = style_class
-        
-        if self.__style_class:
-            if self.__style_class not in Theme.classes:
-                Theme.classes[self.__style_class] = copy.deepcopy(Theme.button)
-            self.__style = Theme.classes[self.__style_class]
-        else:
-            self.__style = Theme.button
     
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(text="{self.__text}")'
     
     def __str__(self) -> str:
         return f'{self.__class__.__name__}("{self.__text}")'
-    
-    @property
-    def style(self) -> dict:
-        return self.__style
-    
-    @style.setter
-    def style(self, style: dict) -> None:
-        self.__style = style
 
     def __draw(self):
+        print(self, self._UI__state.name)
+        state = self._UI__state.name
+
+        text = self._Cell__style[state]['text']
+        background = self._Cell__style[state]['background']
+        border = self._Cell__style[state]['border']
+
+        if self._UI__state.name != 'NORMAL':
+            state = 'NORMAL'
+        
+        radius = self._Cell__style[state]['radius']
+        font = self._Cell__style[state]['font']
+        font_size = self._Cell__style[state]['font-size']
+        pad = self._Cell__style[state]['padding'] * 2
+
+        if self.__text:
+            text = FontRender(
+                self.__text, text, font, font_size,
+                self.width if self.__elided else None, pad)
+            if self.width < text.width + pad: self.width = text.width + pad
+            if self.height < text.height + pad: self.height = text.height + pad
+
+            tx = self._x + (self.width // 2) - (text.width // 2)
+            ty = self._y + (self.height // 2) - (text.height // 2)
+
+        self._Cell__drawer.rect(
+            self._x, self._y, self.width, self.height, border, radius)
+
+        self._Cell__drawer.rect(
+            self._x + 1, self._y +1, self.width - 2,self.height - 2,
+            background, radius - 1)
+
+        if self.__text:
+            self._Cell__drawer.text(tx, ty, text)
+    
+    def __draw_bkp(self):
         print(self, self._UI__state.name)
         state = self._UI__state.name
 
--- cell/cell.py
+++ cell/cell.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
+import copy
+
 from ..flag import Fill
 from ..mixin import Margin, Pos, Size
-from ..ui import UI
+from ..ui import UI, Theme
 
 
 class Cell(Margin, Pos, Size, UI):
     """..."""
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, style_class: str = None, *args, **kwargs) -> None:
         """..."""
         super().__init__(*args, **kwargs)
         self.__drawer = None
         self.__fill = Fill.X
+        self.__style_class = style_class
+
+        theme = getattr(Theme, self.__class__.__name__.lower())  # Theme.button
+        if self.__style_class:
+            if self.__style_class not in Theme.classes:
+                Theme.classes[self.__style_class] = copy.deepcopy(theme)
+            self.__style = Theme.classes[self.__style_class]
+        else:
+            self.__style = theme # Theme.button
+        
+        self.texture_base = None
+        self.texture_hover = None
+        self.needs_rebuild = True
     
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}()'
@@ -26,6 +41,14 @@ class Cell(Margin, Pos, Size, UI):
     @fill.setter
     def fill(self, fill: Fill) -> None:
         self.__fill = fill
+    
+    @property
+    def style(self) -> dict:
+        return self.__style
+    
+    @style.setter
+    def style(self, style: dict) -> None:
+        self.__style = style
     
     def __hit_test(self, x: int, y: int) -> UI | None:
         if not self.visible:
