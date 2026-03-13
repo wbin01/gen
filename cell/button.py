ad4694ce052cb4bc45f31aa7a47142f7c031080d
wbin01 <wildenalisson@outlook.com>
2026-03-13 16:29:50 -0300

    Sep state

--- cell/button.py
+++ cell/button.py
@@ -29,17 +29,17 @@ class Button(Cell):
         self._font = ImageFont.truetype(
             self._font_name, self.style['BASE']['font-size'])
         
-        self._text_x = 0
-        self._text_y = 0
-        self._text_w = 0
-        self._text_h = 0
+        self._txt_x = 0
+        self._txt_y = 0
+        self._txt_w = 0
+        self._txt_h = 0
 
         self._tt_base = None
-        self._tt_base_text = None
+        self._tt_base_txt = None
         self._tt_hover = None
-        self._tt_hover_text = None
+        self._tt_hover_txt = None
         self._tt_pressed = None
-        self._tt_pressed_text = None
+        self._tt_pressed_txt = None
         
         self._tt_width = 0
         self._tt_height = 0
@@ -73,9 +73,9 @@ class Button(Cell):
                 self._font_name, self.style['BASE']['font-size'])
 
         if mode == 'UNIT' and self._invalidate_item:
-            self._build_text_textures('BASE')
-            self._build_text_textures('HOVER')
-            self._build_text_textures('PRESSED')
+            self._build_text_textures(state='BASE')
+            self._build_text_textures(state='HOVER')
+            self._build_text_textures(state='PRESSED')
             self._invalidate_item = None
             self._need_rebuild = True
             
@@ -84,14 +84,14 @@ class Button(Cell):
             self._set_state('BASE')
 
             self._tt_base = self._drawer.reset(self._tt_base)
-            self._tt_base_text = self._drawer.reset(self._tt_base_text)
+            self._tt_base_txt = self._drawer.reset(self._tt_base_txt)
             self._tt_hover = self._drawer.reset(self._tt_hover)
-            self._tt_hover_text = self._drawer.reset(self._tt_hover_text)
+            self._tt_hover_txt = self._drawer.reset(self._tt_hover_txt)
             self._tt_pressed = self._drawer.reset(self._tt_pressed)
-            self._tt_pressed_text = self._drawer.reset(self._tt_pressed_text)
+            self._tt_pressed_txt = self._drawer.reset(self._tt_pressed_txt)
 
-            self._build_background_textures('BASE')
-            self._build_text_textures('BASE')
+            self._build_background_textures(state='BASE')
+            self._build_text_textures(state='BASE')
 
             self._resize_width = 0
             self._resize_height = 0
@@ -110,61 +110,61 @@ class Button(Cell):
                 self._tt_base, int(self._x), int(self._y),
                 self._resize_width, self._resize_height)
             
-            self._text_x = self._x + (self._resize_width//2)-(self._text_w//2)
-            self._text_y = self._y + (self._resize_height//2)-(self._text_h//2)
+            self._txt_x = self._x + (self._resize_width//2)-(self._txt_w//2)
+            self._txt_y = self._y + (self._resize_height//2)-(self._txt_h//2)
 
             self._drawer.set_texture(
-                self._tt_base_text,
-                self._text_x, self._text_y, self._text_w, self._text_h)
+                self._tt_base_txt,
+                self._txt_x, self._txt_y, self._txt_w, self._txt_h)
             return
         
         self._set_textures()
     
-    def _build_background_textures(self, mode: str) -> None:
+    def _build_background_textures(self, state: str) -> None:
         self._tt_width = int(self.width)
         self._tt_height = int(self.height)
 
-        if mode == 'BASE':
+        if state == 'BASE':
             self._tt_base = self._drawer.build_texture(self._tt_base,
                 self._tt_width, self._tt_height, self._build_render, 'BASE')
             if self._log_rebuild: print('BASE bg', self, time.time())
         
-        elif mode == 'HOVER':
+        elif state == 'HOVER':
             self._tt_hover = self._drawer.build_texture(self._tt_hover,
                 self._tt_width, self._tt_height, self._build_render, 'HOVER')
             if self._log_rebuild: print('HOVER bg', self, time.time())
         
-        elif mode == 'PRESSED':
+        elif state == 'PRESSED':
             self._tt_pressed = self._drawer.build_texture(self._tt_pressed,
                 self._tt_width, self._tt_height, self._build_render, 'CLICKED')
             if self._log_rebuild: print('PRESSED bg', self, time.time())
     
-    def _build_text_textures(self, mode: str) -> None:
+    def _build_text_textures(self, state: str) -> None:
         padding = self.style['BASE']['padding'] * 2
         width = int(self.width - padding)
-        if mode == 'BASE':
-            self._tt_base_text, self._text_w, self._text_h = self._drawer.font(
-                self._text, self._tt_base_text, self._font,
+        if state == 'BASE':
+            self._tt_base_txt, self._txt_w, self._txt_h = self._drawer.font(
+                self._text, self._tt_base_txt, self._font,
                 self.style['BASE']['font-size'],
                 self.style['BASE']['font-color'], self._elided, width)
             if self._log_rebuild: print('BASE text', self, time.time())
 
-        elif mode == 'HOVER':
-            self._tt_hover_text, self._text_w, self._text_h = self._drawer.font(
-                self._text, self._tt_hover_text, self._font,
+        elif state == 'HOVER':
+            self._tt_hover_txt, self._txt_w, self._txt_h = self._drawer.font(
+                self._text, self._tt_hover_txt, self._font,
                 self.style['BASE']['font-size'],
                 self.style['HOVER']['font-color'], self._elided, width)
             if self._log_rebuild: print('HOVER text', self, time.time())
 
-        elif mode == 'PRESSED':
-            self._tt_pressed_text, self._text_w, self._text_h = self._drawer.font(
-                self._text, self._tt_pressed_text, self._font,
+        elif state == 'PRESSED':
+            self._tt_pressed_txt, self._txt_w, self._txt_h = self._drawer.font(
+                self._text, self._tt_pressed_txt, self._font,
                 self.style['BASE']['font-size'],
                 self.style['CLICKED']['font-color'], self._elided, width)
             if self._log_rebuild: print('PRESSED text', self, time.time())
         
-        self._text_x = self._x + (self.width // 2) - (self._text_w // 2)
-        self._text_y = self._y + (self.height // 2) - (self._text_h // 2)
+        self._txt_x = self._x + (self.width // 2) - (self._txt_w // 2)
+        self._txt_y = self._y + (self.height // 2) - (self._txt_h // 2)
     
     def _build_render(self, state: str = 'BASE'):
         radius = self.style['BASE']['radius']
@@ -187,31 +187,31 @@ class Button(Cell):
                 int(self._x), int(self._y), int(self.width), int(self.height))
             
             self._drawer.set_texture(
-                self._tt_base_text,
-                self._text_x, self._text_y, self._text_w, self._text_h)
+                self._tt_base_txt,
+                self._txt_x, self._txt_y, self._txt_w, self._txt_h)
 
         elif self._state.value == 'HOVER':
             if not self._tt_hover:
-                self._build_background_textures('HOVER')
-                self._build_text_textures('HOVER')
+                self._build_background_textures(state='HOVER')
+                self._build_text_textures(state='HOVER')
             
             self._drawer.set_texture(
                 self._tt_hover,
                 int(self._x), int(self._y), int(self.width), int(self.height))
             
             self._drawer.set_texture(
-                self._tt_hover_text,
-                self._text_x, self._text_y, self._text_w, self._text_h)
+                self._tt_hover_txt,
+                self._txt_x, self._txt_y, self._txt_w, self._txt_h)
         
         elif self._state.value == 'PRESSED':
             if not self._tt_pressed:
-                self._build_background_textures('PRESSED')
-                self._build_text_textures('PRESSED')
+                self._build_background_textures(state='PRESSED')
+                self._build_text_textures(state='PRESSED')
             
             self._drawer.set_texture(
                 self._tt_pressed,
                 int(self._x), int(self._y), int(self.width), int(self.height))
             
             self._drawer.set_texture(
-                self._tt_pressed_text,
-                self._text_x, self._text_y, self._text_w, self._text_h)
+                self._tt_pressed_txt,
+                self._txt_x, self._txt_y, self._txt_w, self._txt_h)
--- cell/empty.py
+++ cell/empty.py
@@ -45,7 +45,7 @@ class Empty(Cell):
             self._tt_hover = self._drawer.reset(self._tt_hover)
             self._tt_pressed = self._drawer.reset(self._tt_pressed)
 
-            self._build_background_textures('BASE')
+            self._build_background_textures(state='BASE')
             self._resize_width = 0
             self._resize_height = 0
         
@@ -66,21 +66,21 @@ class Empty(Cell):
         
         self._set_textures()
     
-    def _build_background_textures(self, mode: str) -> None:
+    def _build_background_textures(self, state: str) -> None:
         self._tt_width = int(self.width)
         self._tt_height = int(self.height)
 
-        if mode == 'BASE':
+        if state == 'BASE':
             self._tt_base = self._drawer.build_texture(self._tt_base,
                 self._tt_width, self._tt_height, self._build_render, 'BASE')
             if self._log_rebuild: print('BASE bg', self, time.time())
         
-        elif mode == 'HOVER':
+        elif state == 'HOVER':
             self._tt_hover = self._drawer.build_texture(self._tt_hover,
                 self._tt_width, self._tt_height, self._build_render, 'HOVER')
             if self._log_rebuild: print('HOVER bg', self, time.time())
 
-        elif mode == 'PRESSED':
+        elif state == 'PRESSED':
             self._tt_pressed = self._drawer.build_texture(self._tt_pressed,
                 self._tt_width, self._tt_height, self._build_render, 'CLICKED')
             if self._log_rebuild: print('PRESSED bg', self, time.time())
@@ -107,7 +107,7 @@ class Empty(Cell):
 
         elif self._state.value == 'HOVER':
             if not self._tt_hover:
-                self._build_background_textures('HOVER')
+                self._build_background_textures(state='HOVER')
 
             self._drawer.set_texture(
                 self._tt_hover,
@@ -115,7 +115,7 @@ class Empty(Cell):
         
         elif self._state.value == 'PRESSED':
             if not self._tt_pressed:
-                self._build_background_textures('PRESSED')
+                self._build_background_textures(state='PRESSED')
 
             self._drawer.set_texture(
                 self._tt_pressed,
