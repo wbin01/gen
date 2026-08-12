1141fad24cc8f84356bead123de54517be0e2049
wbin01 <wildenalisson@outlook.com>
2026-08-11 23:44:32 -0300

    Col Horizontal Scrollbar

--- layout/viewport.py
+++ layout/viewport.py
@@ -24,8 +24,10 @@ class ViewPort(UIObject):
         self._control_y = self._parent._y
         self._width = None
         self._height = None
-        self._first_roll_up = False
         self._first_roll_down = False
+        self._first_roll_left = False
+        self._first_roll_right = False
+        self._first_roll_up = False
         self._dragging = False
 
         self._cursor_side = None
@@ -102,6 +104,41 @@ class ViewPort(UIObject):
             self._parent._invalidate()
             self._parent._app._render_mode = 'POSITION'
             self._parent._app._render_update = True
+    
+    def _roll_left(self, cursor_x: float, step: float = 0) -> None:
+        self._first_roll_left = True
+        if not self._parent: return
+        if not self._parent._scroll: return
+
+        stop = (
+            cursor_x < self._parent._x,
+            cursor_x > self._parent._x + self.width,
+            self._parent._objects[0]._x > self._parent._x)
+        if any(stop): return
+        
+        if self._parent._objects[0]._x <= self._parent._x:
+            self._control_x += step if step else self._roll_step
+            self._parent._invalidate()
+            self._parent._app._render_mode = 'POSITION'
+            self._parent._app._render_update = True
+    
+    def _roll_right(self, cursor_x: float, step: float = 0) -> None:
+        self._first_roll_right = True
+        if not self._parent: return
+        if not self._parent._scroll: return
+        last_obj = self._parent._objects[-1]
+
+        stop = (
+            cursor_x < self._parent._x,
+            cursor_x > self._parent._x + self.width,
+            last_obj._x + last_obj._width < self._parent._x + self.width)
+        if any(stop): return
+        
+        if last_obj._x + last_obj._width >= self._parent._x + self.width:
+            self._control_x -= step if step else self._roll_step
+            self._parent._invalidate()
+            self._parent._app._render_mode = 'POSITION'
+            self._parent._app._render_update = True
 
     def _roll_up(self, cursor_y: float, step: float = 0) -> None:
         self._first_roll_up = True
@@ -127,6 +164,7 @@ class ViewPort(UIObject):
         h = self._parent._y + self.height + self._parent._padding_y
         px, py = self._parent._padding_x * 2, self._parent._padding_y * 2
 
+        # V Bar
         height_delta = h - self._parent._y
         y_delta = self.y
         if self._parent._objects_height > self.height:
@@ -142,18 +180,28 @@ class ViewPort(UIObject):
 
         self._vbar_rect = w - thickness, bar_y, thickness, height_delta
         
+        # H Bar
         width_delta = w - self._parent._x
         if self._parent._fill_width > self.width:
             d = self._parent._fill_width / (self.width - px)
             width_delta = int(round(self.width / d))
 
-        self._hbar_rect = self._parent._x, h-thickness, width_delta, thickness
+            x_delta = self.x - (self.x + self._control_x)
+            x_delta = x_delta // d
+        
+        bar_x = self._parent._x + x_delta
+        if bar_x + width_delta > w: bar_x = w - width_delta
+        if bar_x < self._parent._x: bar_x = self._parent._x
 
+        self._hbar_rect = bar_x, h-thickness, width_delta, thickness
+
+        # Side
         if h - thickness < cursor_y < h and self._parent._x < cursor_x < w:
             self._cursor_side, self._scroll_side = 'H', 'H'
         elif w - thickness < cursor_x < w and self._parent._y < cursor_y < h:
             self._cursor_side, self._scroll_side = 'V', 'V'
         
+        # Block
         if (self._cursor_side == 'H' and
                 self._parent._fill_width + px <= self._parent.width or
                 self._cursor_side == 'V' and
@@ -184,6 +232,15 @@ class ViewPort(UIObject):
                 self._roll_down(cursor_y, self._vbar_rect[1] - point)
             elif cursor_y > self._offset[1]:
                 self._roll_up(cursor_y, point - self._vbar_rect[1])
+        else:
+            if not self._cursor_point:
+                self._cursor_point = cursor_x - self._hbar_rect[0]
+            point = (cursor_x - self._cursor_point)
+
+            if cursor_x < self._offset[0]:
+                self._roll_left(cursor_x, self._hbar_rect[0] - point)
+            elif cursor_x > self._offset[0]:
+                self._roll_right(cursor_x, point - self._hbar_rect[0])
 
         self._offset = cursor_x, cursor_y
 
